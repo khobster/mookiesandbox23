5719574702b935a7eb2951384567e10370359937
khobster <152924497+khobster@users.noreply.github.com>
2024-08-21 14:59:24 -0400

    Update plunko.js

--- plunko.js
+++ plunko.js
@@ -446,13 +446,13 @@ function displayPlayerFromDecade(decade) {
             playerDecade = '1970s';
         } else if (playerYear >= 80 && playerYear <= 89) {
             playerDecade = '1980s';
-        } else if (playerYear >= 90 && playerYear <= 99) {
+        } else if (playerYear >= 90 and playerYear <= 99) {
             playerDecade = '1990s';
-        } else if (playerYear >= 0 && playerYear <= 9) {
+        } else if (playerYear >= 0 and playerYear <= 9) {
             playerDecade = '2000s';
-        } else if (playerYear >= 10 && playerYear <= 19) {
+        } else if (playerYear >= 10 and playerYear <= 19) {
             playerDecade = '2010s';
-        } else if (playerYear >= 20 && playerYear <= 29) {
+        } else if (playerYear >= 20 and playerYear <= 29) {
             playerDecade = '2020s';
         }
 
