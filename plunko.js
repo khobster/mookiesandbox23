dc284532ea35fbc768156bb7cbaca5a8146b9114
khobster <152924497+khobster@users.noreply.github.com>
2024-08-20 21:56:49 -0400

    Update plunko.js

--- plunko.js
+++ plunko.js
@@ -454,19 +454,19 @@ function displayPlayerFromDecade(decade) {
         let playerDecade;
         if (playerYear >= 50 && playerYear <= 59) {
             playerDecade = '1950s';
-        } else if (playerYear >= 60 and playerYear <= 69) {
+        } else if (playerYear >= 60 && playerYear <= 69) {
             playerDecade = '1960s';
-        } else if (playerYear >= 70 and playerYear <= 79) {
+        } else if (playerYear >= 70 && playerYear <= 79) {
             playerDecade = '1970s';
-        } else if (playerYear >= 80 and playerYear <= 89) {
+        } else if (playerYear >= 80 && playerYear <= 89) {
             playerDecade = '1980s';
-        } else if (playerYear >= 90 and playerYear <= 99) {
+        } else if (playerYear >= 90 && playerYear <= 99) {
             playerDecade = '1990s';
-        } else if (playerYear >= 0 and playerYear <= 9) {
+        } else if (playerYear >= 0 && playerYear <= 9) {
             playerDecade = '2000s';
-        } else if (playerYear >= 10 and playerYear <= 19) {
+        } else if (playerYear >= 10 && playerYear <= 19) {
             playerDecade = '2010s';
-        } else if (playerYear >= 20 and playerYear <= 29) {
+        } else if (playerYear >= 20 && playerYear <= 29) {
             playerDecade = '2020s';
         }
 
